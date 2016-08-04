eb3c6b89858401a351e84128e4c6e7c0fbed9589
mbr84 <matthew.bramfeld@gmail.com>
2016-08-04 00:01:03 -0700

    trying to fix heroku database

--- db/migrate/20160704184103_create_rewards.rb
+++ db/migrate/20160704184103_create_rewards.rb
@@ -2,7 +2,7 @@ class CreateRewards < ActiveRecord::Migration
   def change
     create_table :rewards do |t|
       t.string :name, null: false
-      t.integer :value, null: false
+      t.integer :cost, null: false
       t.text :description, null: false
       t.integer :project_id, null: false
 
--- db/migrate/20160710221540_fix_column_name.rb
+++
@@ -1,5 +0,0 @@
-class FixColumnName < ActiveRecord::Migration
-  def change
-    rename_column :rewards, :value, :cost
-  end
-end

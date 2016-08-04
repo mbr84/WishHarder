310febe589129dc699ad44123a94b6ed0f84f484
mbr84 <matthew.bramfeld@gmail.com>
2016-08-03 22:38:28 -0700

    added index to fixed column

--- db/migrate/20160804053414_add_index_to_column.rb
+++ db/migrate/20160804053414_add_index_to_column.rb
@@ -1,4 +1,5 @@
 class AddIndexToColumn < ActiveRecord::Migration
   def change
+    add_index :rewardings, :customer_id, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804044658) do
+ActiveRecord::Schema.define(version: 20160804053414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20160804044658) do
   end
 
   add_index "rewardings", ["backer_id"], name: "index_rewardings_on_backer_id", using: :btree
+  add_index "rewardings", ["customer_id"], name: "index_rewardings_on_customer_id", unique: true, using: :btree
   add_index "rewardings", ["reward_id"], name: "index_rewardings_on_reward_id", using: :btree
 
   create_table "rewards", force: :cascade do |t|

2a3d5dbd672bb9c7b6945c9164831cbc362dbd02
mbr84 <matthew.bramfeld@gmail.com>
2016-08-07 16:37:00 -0700

    sort rewards

--- frontend/components/rewards_sidebar.jsx
+++ frontend/components/rewards_sidebar.jsx
@@ -4,11 +4,13 @@ const RewardSidebarItem = require('./reward_sidebar_item')
 
 const RewardSidebar = React.createClass({
   render(){
-
+    debugger
     return (
       <div>
         <ul>
-          {this.props.rewards.map(reward => {
+          {this.props.rewards.sort((x, y) => {
+            return x.cost - y.cost
+          }).map(reward => {
             return <li key={reward.reward_id}><RewardSidebarItem reward={reward} /></li>
           })}
         </ul>
